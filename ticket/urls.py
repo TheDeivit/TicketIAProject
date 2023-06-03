2bb7beead46adb66e3598c0643754ab9f299d415
thedeivit <da.059@hotmail.com>
2023-06-03 13:28:40 -0600

    Se agregan vistas dependiendo el usuario

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name='ticket' 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('ticket/add', views.add, name='add'),
     path('ticket/update/<str:pk>', views.update, name='update'),
     path('ticket/delete/<str:pk>', views.delete, name='delete'),
-    path('ticket/j-get-ticket-by-id/<str:pk>', views.jgetTicketId)
+    path('ticket/j-get-ticket-by-id/<str:pk>', views.jgetTicketId),
+    # Ruta para la vista de inicio de sesión personalizada
+    path('login', views.custom_login, name='login'),
+    # Ruta para la vista de cierre de sesión
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    #Ruta para la vista inicial
+    path('', views.landingpage, name='landing'),
+    path('create_ticket', views.create_ticket, name='create_ticket'),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -7,6 +7,7 @@ from bson.objectid import ObjectId
 from .models import Ticket
 from .forms import TicketForm
 from bson import ObjectId
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -24,7 +25,7 @@ def add(request):
         else:
             return _listTicket(request, form)
         
-    return redirect('ticket:index')
+    return redirect('ticket:create_ticket')
 
 def update(request, pk):
 
@@ -50,6 +51,31 @@ def delete(request,pk):
     
     return redirect('ticket:index')
     
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        print(username)
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Autenticación exitosa
+            login(request, user)
+            if user.groups.filter(name='Solicitantes').exists():
+                print('Es solicitante')
+                # El usuario pertenece al grupo "Solicitantes"
+                # Realiza las acciones correspondientes para los solicitantes
+                return redirect('ticket:create_ticket')
+            else:
+                print('Es admin')
+                # El usuario no pertenece al grupo "Solicitantes"
+                # Realiza otras acciones para usuarios que no sean solicitantes
+                return redirect('/admin')
+        else:
+            # Autenticación fallida
+            return render(request, 'ticket/login.html', {'error': 'Credenciales inválidas'})
+    else:
+        return render(request, 'ticket/login.html')
+    
 #PRIVATE
 
 def _listTicket(request, form):
@@ -61,6 +87,12 @@ def _listTicket(request, form):
 
     return render(request, 'ticket/index.html', {'tickets':tickets_page, 'form': form})
 
+def landingpage(request):
+    return render(request,'ticket/landingpage.html')
+
+def create_ticket(request):
+    form = TicketForm()  # Crear una instancia del formulario
+    return render(request, 'ticket/create_ticket.html', {'form': form})
 
 #------------ JSON
 
