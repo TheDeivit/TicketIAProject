6f586d3025660ef8eae21c9eeaeafbb13ce2de92
thedeivit <da.059@hotmail.com>
2023-09-20 23:18:58 -0600

    Se agregan ALLOWED HOSTS

--- TicketIA/settings.py
+++ TicketIA/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'f+-cn3t36*mzi3^a+jjs53$xe%9d!9+9zu00kj-dkp6^&$!vvl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.232.57.26', '18.232.57.26:8000']
 
 
 # Application definition
